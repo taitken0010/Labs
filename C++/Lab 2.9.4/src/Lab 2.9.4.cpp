6ef4114f75d87d2e5e32197abe5779a3e71bb156
Taylan Aitken <taitken0010@vbtc.net>
2018-11-20 13:58:18 -0500

    lab 2.9.4

---
+++ C++/Lab 2.9.4/src/Lab 2.9.4.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : 4.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	double vector[] = { 1., 2., 3., 4., 5.};
+	int n = sizeof(vector) / sizeof(vector[0]);
+	double arith, harm, geo, root, x;
+	for(int i = 0; i < n; i++){
+
+	}
+
+	cout << "Arithmetic Mean = " << arith << endl;
+	cout << "Harmonic Mean = " << harm << endl;
+	cout << "Geometric Mean = " << geo << endl;
+	cout << "Root Mean Square = " << root << endl;
+	return 0;
+}
